9937195748e29577987abec457ed11ae8b27d828
armen <armenmerikyan@gmail.com>
2021-01-21 14:37:29 -0800

    initial build

---
+++ rungenerateto.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# My first script
+while [ 1 ]
+do
+        echo "Running Generate to Address Command in a loop"
+        ./brownie-cli generatetoaddress 1000 MdhFygq5KJJdYiVrkK6HDx1DHnTevantcM
+done
