fc79ccd0bd71bad29c411f33c9b60b89845ebae7
armen <armenmerikyan@gmail.com>
2021-01-22 05:06:58 -0800

    initial build

--- rungenerateto.sh
+++ rungenerateto.sh
@@ -4,5 +4,6 @@
 while [ 1 ]
 do
         echo "Running Generate to Address Command in a loop"
-        ./brownie-cli generatetoaddress 1000 MdhFygq5KJJdYiVrkK6HDx1DHnTevantcM
+        ./src/maximcoin-cli generatetoaddress 1000 MGps5cEGF19jyuorApMu3foaaTQ8XqmLxQ
+        sleep 10
 done
