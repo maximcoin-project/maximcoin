55f387477f2f579ef4146090ad48ab0fa29935dd
Maximcoin <77675245+maximcoin-project@users.noreply.github.com>
2021-03-31 00:47:12 -0700

    Update setup_aws.sh

--- setup_aws.sh
+++ setup_aws.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# setup all Dependencies
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libboost-all-dev libboost-program-options-dev
+sudo apt-get install -y libminiupnpc-dev libzmq3-dev libprotobuf-dev protobuf-compiler unzip software-properties-common
+sudo apt-get install -y libboost-all-dev libdb++-dev libminiupnpc-dev
+sudo apt-get install -y libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+sudo apt-get install -y libqrencode-dev
+./contrib/install_db4.sh `pwd`
+export BDB_PREFIX='/home/ubuntu/maximcoin/db4'
+chmod 777 autogen.sh
+./autogen.sh
+./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include"
+make
